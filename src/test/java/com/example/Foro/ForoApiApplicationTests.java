417687cfd1cd1508e37ad9972b59674671849fe3
JuanRuales <juancarcaicedo@unicauca.edu.co>
2024-12-30 14:56:16 -0500

    first commit

---
+++ src/test/java/com/example/Foro/ForoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
